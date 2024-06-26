b4e94d8fe66e9cb2ea824d1ec6ad61448a740e65
APAC\HYN4HC <Nhi.NguyenThiHuyen@vn.bosch.com>
2024-06-26 17:10:52 +0700

    modify pytest file

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,11 @@
-import pytest
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import app
+
+# Your tests follow here
+
 
 @pytest.fixture
 def client():
