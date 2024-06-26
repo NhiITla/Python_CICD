08ebee899472cced8c924a69e38a969098ecf491
APAC\HYN4HC <Nhi.NguyenThiHuyen@vn.bosch.com>
2024-06-26 17:19:32 +0700

    debug

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,10 @@
-import pytest
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import app
+
+import pytest
 
 @pytest.fixture
 def client():
